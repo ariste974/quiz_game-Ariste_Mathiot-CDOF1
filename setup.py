090676d9145ce62cc4170f0f47481815cb7c334d
ariste974 <aristemathiot@gmail.com>
2025-01-13 14:10:35 +0100

    Ajout du code

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+ name='{quiz_game-Ariste_Mathiot-CDOF1
+}',
+ version='1.0',
+ author='{ariste974}',
+ license='MIT',
+ long_description=open('README.md').read(),
+)
